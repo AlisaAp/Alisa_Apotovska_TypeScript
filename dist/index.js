4c0d29d209d0a2c6b9af2fb5b96cc5a0f1a02c8e
Alisa Apotovska <masanaapot@gmail.com>
2023-11-21 19:59:16 +0200

    homework_5

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+function checkObjValues(obj, checkValue) {
+    for (const key in obj) {
+        // eslint-disable-next-line no-prototype-builtins
+        if (obj.hasOwnProperty(key) && typeof obj[key] !== "number") {
+            return false;
+        }
+        if (obj[key] !== checkValue) {
+            return false;
+        }
+    }
+    return true;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,46 @@
 //1
 interface IUnionInterface {
-  [key: string]: number | string;
+    [key: string]: number | string;
 }
+
 //2
 interface IFunctionInterface {
-  [key: string]: (...args: any[]) => any;
+    [key: string]: (...args: any[]) => any;
 }
+
 //3
 interface IArrayInterface {
-  [key: number]:  string;
+    [key: number]: string;
 }
 
 //4
 interface IInterface {
-  name: string;
-  [key: string]: any;
+    name: string;
+
+    [key: string]: any;
 }
+
 //5
+interface ISomeInterface {
+    [key: string]: number;
+}
+
+interface IExtendedInterface extends ISomeInterface {
+    someProperty: number;
+}
 
 //6
 interface Interface {
-  [key: string]:  number;
+    [key: string]: number;
 }
-function checkValues(obj: Interface): boolean {
+
+function checkObjValues(obj: Interface, checkValue: number): boolean {
     for (const key in obj) {
+        // eslint-disable-next-line no-prototype-builtins
         if (obj.hasOwnProperty(key) && typeof obj[key] !== "number") {
+            return false;
+        }
+        if (obj[key] !== checkValue) {
             return false;
         }
     }
